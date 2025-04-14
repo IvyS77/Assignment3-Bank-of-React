7a62206258ee1023c59ba1b9cb20b630391c275e
IvyS <ivysg0817@gmail.com>
2025-04-14 11:48:33 -0400

    update,App.js

---
+++ src/components/AccountDetails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AccountDetails(props) {
+  return (
+    <div>
+      <h2>Account Details</h2>
+      <p>This area displays detailed account information…</p>
+    </div>
+  );
+}
+
+export default AccountDetails;
